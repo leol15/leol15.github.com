592e344c81498983567da7c83269663ac00893d2
Leo Liao <41461222+leol15@users.noreply.github.com>
2018-07-23 15:18:01 -0700

    mb

--- LeoCleanSite/MyJavaScript.js
+++ LeoCleanSite/MyJavaScript.js
@@ -1,15 +1,21 @@
 
 var informText = function(txt) {
 	var el = document.getElementById("inform_content");
-	el.textContent = txt;
-	setTimeout(function() {removeText()}, 1500);
+	el.style.animationName = "rotY90";
+	window.setTimeout(function() {el.textContent = txt; 
+		el.style.animationName = "rotY-90";}, 1000);
+	window.setTimeout(function() {removeText()}, 3500);
 };
 var removeText = function() {
 	var el = document.getElementById("inform_content");
-	el.textContent = "leo";
+	el.style.animationName = "rotY90";
+	window.setTimeout(function() {el.textContent = "leo";
+	el.style.animationName = "rotY-90";}, 1000);
+	//window.setTimeout(function() {el.style.animationName = ""}, 1000);
 };
 
 
 var headerCenterMarkEl = document.getElementById("center_mark");
 var welText = "Welcome to my world";
 headerCenterMarkEl.addEventListener("mouseover", informText.bind(null, welText));
+
