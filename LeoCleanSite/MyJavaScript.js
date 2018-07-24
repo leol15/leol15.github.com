59dbe464d0541f96de68af08de52bd451f86d6ba
Leo Liao <41461222+leol15@users.noreply.github.com>
2018-07-23 17:57:54 -0700

    mb

--- LeoCleanSite/MyJavaScript.js
+++ LeoCleanSite/MyJavaScript.js
@@ -21,17 +21,27 @@ var removeText = function() {
 	el.style.animationName = "rotY-90";}, 1000);
 };
 
-
+//header touch event
 var welText = "Welcome to my world";
 var headerCenterMarkEl = document.getElementById("center_mark");
 headerCenterMarkEl.addEventListener("touchstart", informText.bind(null, welText));
 
+//left icon touch event
 var leftIconBox = document.getElementById("left_icon");
+var li_a = document.getElementById("li_a");
+var li_b = document.getElementById("li_b");
+var li_c = document.getElementById("li_c");
 var leftIconClicked = function() {
 	if(leftIconBox.className == "left_icon_on") {
 		leftIconBox.className = "left_icon_off";
+		li_a.className = "li_a_off";
+		li_b.className = "li_b_off";
+		li_c.className = "li_c_off";
 	} else {
 		leftIconBox.className = "left_icon_on";
+		li_a.className = "li_a_on";
+		li_b.className = "li_b_on";
+		li_c.className = "li_c_on";
 	}
 }; 
 leftIconBox.addEventListener("touchend", leftIconClicked);
