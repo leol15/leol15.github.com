2e182a0bebed23e453c5d6a4bf368d5dabfcae7c
Leo Liao <41461222+leol15@users.noreply.github.com>
2018-09-02 11:27:19 -0700

    +1

--- SoundInHTML/myScript.js
+++ SoundInHTML/myScript.js
@@ -63,8 +63,14 @@ var changeHeading = function(e) {
 		newHeadingEl.value = "";
 
 		var req = new XMLHttpRequest();
-		req.onreadystatechange = function() {};
 		req.open("POST", "secret.txt", true);
+
+		req.onreadystatechange = function() {
+			if(this.readyState == 4 && this.status == 200) {
+				displayAreaEl.innerHTML = this.responseText;
+			}
+		};
+		req.setRequestHeader('Content-Type', 'text/txt');
 		req.send("Whart is this about");
 	}
 };
